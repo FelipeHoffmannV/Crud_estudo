8d3470b455743cd77a0e5732ff6431ecd8d3e559
Felipe <hoffmannfelipe940@gmail.com>
2025-10-11 18:01:29 -0300

    implementa ações de adicionar e remover usuários no CRUD

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
+
 from flask import Flask,render_template
 from routes.users_route import user_route
 from database.fic_data import users
 
 app = Flask(__name__)
 
-
-# This route just render the index template.
-@app.route('/')
-def default_route_for_index():
-    return render_template('index.html')
 
 
 app.register_blueprint(user_route)
--- routes/users_route.py
+++ routes/users_route.py
@@ -1,23 +1,29 @@
-from flask import Flask, Blueprint, render_template, request,redirect
+from flask import Flask, Blueprint, render_template, request,redirect, url_for
 from database.fic_data import users
 
-user_route = Blueprint('add_user_route', __name__, template_folder='templates')
+user_route = Blueprint('user_route', __name__, template_folder='templates')
 
-id_user = 0
+@user_route.route('/')
+def show_user():
+    return render_template('index.html', users=users)
+
 @user_route.route('/add', methods=['POST'])
 def add_user():
-    global id_user
-    id_user += 1
     name = request.form['name']
     email = request.form['email']
-
-    user = {'id': id_user,
+    new_id = users[-1]['id'] + 1 if users else 1
+    user = {'id': new_id,
             'name': name, 
             'email': email,
             }
     users.append(user.copy())
     print(users)
-    return render_template('index.html', users=users)
+    return redirect(url_for('user_route.show_user'))
 
 
-    
+@user_route.route('/remove/<int:id>')
+def remove_user(id):
+    for user in users:
+        if id == user['id']:
+            users.remove(user)  
+    return redirect(url_for('user_route.show_user'))
